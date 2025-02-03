cdfced3248ad6756873a6c713e72c262c6cd93cc
anju <anjutmc@gmail.com>
2025-02-03 19:47:54 +0530

    add all files

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	// Render the home html page from static folder
+	http.ServeFile(w, r, "static/home.html")
+}
+
+func contentsPage(w http.ResponseWriter, r *http.Request) {
+	// Render the contents html page
+	http.ServeFile(w, r, "static/contents.html")
+}
+
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	// Render the contact html page
+	http.ServeFile(w, r, "static/contact.html")
+}
+
+func main() {
+
+	http.HandleFunc("/home", homePage)
+	http.HandleFunc("/contents", contentsPage)
+	http.HandleFunc("/contact", contactPage)
+
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
